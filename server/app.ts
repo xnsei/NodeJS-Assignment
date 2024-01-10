6a9d0e84c0d2b8a451840f2f58aa24d64356f017
Tanmay Narayan Banage <tbanage@j5fkwj5wqm.agoda.local>
2024-01-10 11:41:15 +0530

    Refactor: cleaning code

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,5 @@
 import express, {Request, Response} from "express";
 import cors from "cors";
-import data from "./data.json";
 
 
 const app = express();
@@ -20,24 +19,35 @@ interface OutputData {
 
 const calculateData = (data: ImageData[]) => {
     const dateMap: Record<string, number[]> = {};
-    data.forEach((image: ImageData) => {
-        const formattedDate = new Date(image.createdAt).toISOString().split("T")[0];
-        if (dateMap[formattedDate]) {
-            dateMap[formattedDate].push(image.timeToGenerate);
-        } else {
-            dateMap[formattedDate] = [image.timeToGenerate];
-        }
-    });
+    try {
+        data.forEach((image: ImageData) => {
+            if (!image.createdAt || !image.timeToGenerate) {
+                throw new Error("Invalid ImageData format: missing required properties.");
+            }
+            const formattedDate = new Date(image.createdAt).toISOString().split("T")[0];
+            if (dateMap[formattedDate]) {
+                dateMap[formattedDate].push(image.timeToGenerate);
+            } else {
+                dateMap[formattedDate] = [image.timeToGenerate];
+            }
+        });
+    } catch (error) {
+        console.error("Error processing ImageData:", error);
+        return [];
+    }
     const averageData: OutputData[] = [];
     Object.entries(dateMap).forEach(([date, times]) => {
         const averageTime = times.reduce((sum, time) => sum + time, 0) / times.length;
-        averageData.push({ date, numberOfImages: times.length , avgTimeToGenerate: averageTime });
+        averageData.push({date, numberOfImages: times.length, avgTimeToGenerate: averageTime});
     });
     return averageData;
 }
 
 app.get("/data", (req: Request, res: Response) => {
-    res.send(calculateData(data));
+    const data = require("./data.json");
+    res.json({
+        data: JSON.stringify(calculateData(data))
+    });
 });
 
 app.listen(4000, () => {
